8905df0fa29bb9a8a56da51b9ee4afe24268d1ad
azcoigreach <azcoigreach@gmail.com>
2022-02-15 21:13:34 -0700

    bytes

--- skippytel/commands/cmd_counter.py
+++ skippytel/commands/cmd_counter.py
@@ -45,7 +45,7 @@ def cli(ctx):
         for count in range(0,1000,1):
             
             display.text(str(count), 35, 0, 1)
-            count_bytes = bytes(count,"utf-8")
+            count_bytes = bytes(str(count),"utf-8")
             rfm9x.send(count_bytes)
             display.show()
             time.sleep(1)
