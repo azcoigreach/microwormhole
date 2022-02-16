1fc72541e9c5e42358528ccd2bd38011b2cce868
azcoigreach <azcoigreach@gmail.com>
2022-02-16 13:33:47 -0700

    fix float

--- skippytel/commands/cmd_cpu.py
+++ skippytel/commands/cmd_cpu.py
@@ -45,9 +45,9 @@ def cli(ctx):
     while True:
         display.fill(0)
         l1, l2, l3 = psutil.getloadavg()
-        CPU_use = (l3/os.cpu_count()) * 100
-        display.text("CPU:" + float(CPU_use), 35, 0, 1)
-        bytes_out = bytes(str("CPU:" + float(CPU_use)),"utf-8")
+        CPU_use = str((l3/os.cpu_count()) * 100)
+        display.text("CPU:" + CPU_use, 35, 0, 1)
+        bytes_out = bytes(str("CPU:" + CPU_use),"utf-8")
         rfm9x.send(bytes_out)
         display.show()
         time.sleep(1)
